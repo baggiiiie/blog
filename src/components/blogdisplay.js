864fa88f2804711af7bdfbe020c98dc00e6e21a5
baggiiiie <yingchao.dai22@gmail.com>
2024-01-02 22:46:55 +0800

    improve mobile view

--- src/components/blogdisplay.js
+++ src/components/blogdisplay.js
@@ -6,21 +6,16 @@ export default function BlogPage({ allPostsData }) {
 	return (
 		// page div
 		<div className="pageBase">
-			{/* display sheet div */}
 			<div className="blogSheet">
-				{/* header div */}
 				<div className="blogHeader">
 					<h1 className="blogTitle">My thoughts and gibberish</h1>
 					<h2 className="belowTitle">This is your hackernews style blog</h2>
 				</div>
-				{/* content div */}
-				{/* individual blog div */}
 
 				{allPostsData.map(({ id, date, title }) => (
 					<Link href={`/${id}`} key={id}>
-						<div className="flex justify-between hover:underline hover:bg-blue-100 px-4 hover:cursor-pointer">
+						<div className="blogListItem">
 							<p>{title}</p>
-
 							<div className="text-right">
 								{format(parseISO(date), "MMM dd, yyyy")}
 							</div>
